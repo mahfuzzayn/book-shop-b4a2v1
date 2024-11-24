3914bc1ca079f842d3f6a0fcb47a970da62f2309
mahfuzzayn <mahfuzzayn8@gmail.com>
2024-11-24 21:17:05 +0600

    Added Home Page: Welcome route with beautiful json data

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 import { ProductRoutes } from './app/modules/product/product.route'
 import { OrderRoutes } from './app/modules/order/order.route'
+import path from 'path'
+import fs from 'fs'
 const app: Application = express()
 
 // parser
@@ -11,5 +13,27 @@ app.use(cors())
 // application routes
 app.use('/api/products', ProductRoutes)
 app.use('/api/orders', OrderRoutes)
+
+// homepage routes
+app.get('/', (req: Request, res: Response) => {
+    const filePath = path.join(
+        __dirname,
+        'app/config',
+        'homepage.json',
+    )
+
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).json({
+                message: 'Error loading homepage data',
+                success: false,
+                error: err.message,
+            })
+        }
+
+        const jsonData = JSON.parse(data)
+        res.status(200).json(jsonData)
+    })
+})
 
 export default app
